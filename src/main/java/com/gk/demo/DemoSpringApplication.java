3efaeaf87a0305938ccf69ebc74316c591f3a0ab
unknown <govindkumar96@gmail.com>
2024-11-11 23:39:31 +0530

    Added rest end point

--- src/main/java/com/gk/demo/DemoSpringApplication.java
+++ src/main/java/com/gk/demo/DemoSpringApplication.java
@@ -2,12 +2,20 @@ package com.gk.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String test(){
+		return "Hello World !!";
 	}
 
 }
